e098d3e5f724559429660ec1e6a87f88cfc4a360
Faisal KC <faisalkc4u@gmail.com>
2021-07-08 11:05:48 +0400

    intial

---
+++ android/app/src/main/kotlin/com/mvpapps/videocalling/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mvpapps.videocalling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+// import 'dart:async';
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // Try running your application with "flutter run". You'll see the
+//         // application has a blue toolbar. Then, without quitting the app, try
+//         // changing the primarySwatch below to Colors.green and then invoke
+//         // "hot reload" (press "r" in the console where you ran "flutter run",
+//         // or simply save your changes to "hot reload" in a Flutter IDE).
+//         // Notice that the counter didn't reset back to zero; the application
+//         // is not restarted.
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key}) : super(key: key);
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   final Completer<WebViewController> _controller =
+//       Completer<WebViewController>();
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Enable hybrid composition.
+//     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Flutter WebView example'),
+//         // This drop down menu demonstrates that Flutter widgets can be shown over the web view.
+//         actions: <Widget>[],
+//       ),
+//       // We're using a Builder here so we have a context that is below the Scaffold
+//       // to allow calling Scaffold.of(context) so we can show a snackbar.
+//       body: Builder(builder: (BuildContext context) {
+//         return WebView(
+//           initialUrl: 'https://demo.bigbluebutton.org/gl/vid-sgo-1bv-iy6',
+//           javascriptMode: JavascriptMode.unrestricted,
+//           onWebViewCreated: (WebViewController webViewController) {
+//             _controller.complete(webViewController);
+//           },
+//           onProgress: (int progress) {
+//             print("WebView is loading (progress : $progress%)");
+//           },
+//           javascriptChannels: <JavascriptChannel>{
+//             _toasterJavascriptChannel(context),
+//           },
+//           navigationDelegate: (NavigationRequest request) {
+//             if (request.url.startsWith('https://www.youtube.com/')) {
+//               print('blocking navigation to $request}');
+//               return NavigationDecision.prevent;
+//             }
+//             print('allowing navigation to $request');
+//             return NavigationDecision.navigate;
+//           },
+//           onPageStarted: (String url) {
+//             print('Page started loading: $url');
+//           },
+//           onPageFinished: (String url) {
+//             print('Page finished loading: $url');
+//           },
+//           gestureNavigationEnabled: true,
+//         );
+//       }),
+//     );
+//   }
+
+//   JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+//     return JavascriptChannel(
+//         name: 'Toaster',
+//         onMessageReceived: (JavascriptMessage message) {
+//           // ignore: deprecated_member_use
+//           Scaffold.of(context).showSnackBar(
+//             SnackBar(content: Text(message.message)),
+//           );
+//         });
+//   }
+// }
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.camera.request();
+  await Permission.microphone.request();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: InAppWebViewPage());
+  }
+}
+
+class InAppWebViewPage extends StatefulWidget {
+  @override
+  _InAppWebViewPageState createState() => new _InAppWebViewPageState();
+}
+
+class _InAppWebViewPageState extends State<InAppWebViewPage> {
+  InAppWebViewController _webViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("InAppWebView")),
+        body: Container(
+            child: Column(children: <Widget>[
+          Expanded(
+            child: Container(
+              child: InAppWebView(
+                  initialUrlRequest: URLRequest(
+                      url: Uri.parse(
+                          "https://demo.bigbluebutton.org/gl/vid-sgo-1bv-iy6")),
+                  initialOptions: InAppWebViewGroupOptions(
+                    crossPlatform: InAppWebViewOptions(
+                      mediaPlaybackRequiresUserGesture: false,
+                    ),
+                  ),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    _webViewController = controller;
+                  },
+                  androidOnPermissionRequest:
+                      (InAppWebViewController controller, String origin,
+                          List<String> resources) async {
+                    return PermissionRequestResponse(
+                        resources: resources,
+                        action: PermissionRequestResponseAction.GRANT);
+                  }),
+            ),
+          ),
+        ])));
+  }
+}
